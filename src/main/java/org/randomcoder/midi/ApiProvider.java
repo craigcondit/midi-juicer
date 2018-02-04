45189f21bd5c4fea1838dd200c7c904a41734a25
Craig Condit <ccondit@randomcoder.org>
2018-02-03 20:19:54 -0600

    Got MIDI sending working

--- src/main/java/org/randomcoder/midi/ApiProvider.java
+++ src/main/java/org/randomcoder/midi/ApiProvider.java
@@ -36,19 +36,6 @@ public class ApiProvider {
 				.filter(d -> d.getMaxReceivers() != 0)
 				.collect(Collectors.toList());
 
-		for (MidiDevice device : devices) {
-			System.out.printf("Name: '%s'%n", device.getDeviceInfo().getName());
-			System.out.printf("  vendor: '%s'%n", device.getDeviceInfo().getVendor());
-			System.out.printf("  description: '%s'%n", device.getDeviceInfo().getDescription());
-			System.out.printf("  version: '%s'%n", device.getDeviceInfo().getVersion());
-			System.out.printf("  MacMidi device: %s%n", MacMidi.isMacMidiDevice(device));
-			System.out.printf("  class: %s%n", device.getClass());
-			System.out.printf("  maxReceivers: %d%n", device.getMaxReceivers());
-			System.out.printf("  maxTransmitters: %d%n", device.getMaxTransmitters());
-			System.out.printf("  isOpen: %s%n", device.isOpen());
-			System.out.printf("  microSecondPosition: %d%n", device.getMicrosecondPosition());
-		}
-
 		if (devices.isEmpty()) {
 			System.out.println("No matching devices found");
 			return;
@@ -60,7 +47,7 @@ public class ApiProvider {
 		int minNote = 60;
 		int maxNote = 72;
 		int delayMs = 250;
-		
+
 		for (int i = minNote; i <= maxNote; i++) {
 			// send note off for previous note / note on for current one
 			MidiMessage noteOff = new ShortMessage(ShortMessage.NOTE_OFF, 0, Math.max(0, i - 1), 0);
@@ -80,7 +67,7 @@ public class ApiProvider {
 				receiver.send(noteOff2, device.getMicrosecondPosition());
 			}
 		}
-		
+
 		receiver.close();
 		device.close();
 	}
